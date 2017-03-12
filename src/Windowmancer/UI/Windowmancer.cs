f29597859958f668fa6c34346108f651c9315816
tabeyti <tabeyti@gmail.com>
2017-03-12 13:26:12 -0700

    Added profile select via system tray context menu along with synchronization between editor and system tray context menu.
    Better data binding between profile manager and window manager.
    Updated icon with black outline for visibility on white window titles.

--- src/Windowmancer/UI/Windowmancer.cs
+++ src/Windowmancer/UI/Windowmancer.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿// TODO: Prevent multiple MainForm from being spawned
+
+using System;
 using System.Windows.Forms;
 using Microsoft.Practices.Unity;
 using Windowmancer.Configuration;
 using Windowmancer.Properties;
 using Windowmancer.Services;
+using System.Collections.Generic;
+using Windowmancer.Models;
 
 namespace Windowmancer.UI
 {
@@ -14,14 +18,14 @@ namespace Windowmancer.UI
     private KeyHookManager _keyHookManager;
     private readonly ProfileManager _profileManager;
     private readonly WindowManager _windowManager;
-
     private ContextMenu _traContextMenu;
+    private MainForm _editor;
 
     public Windowmancer(IUnityContainer serviceResolver)
     {
       _serviceResolver = serviceResolver;
-      _profileManager = new ProfileManager(_serviceResolver.Resolve<ProfileManagerConfig>());
       _windowManager = new WindowManager();
+      _profileManager = new ProfileManager(_serviceResolver.Resolve<ProfileManagerConfig>(), _windowManager);
       Initialize();
     }
 
@@ -33,19 +37,20 @@ namespace Windowmancer.UI
 
     private void Initialize()
     {
-      _windowManager.LoadProfile(_profileManager.ActiveProfile);
       _keyHookManager = new KeyHookManager(OnKeyCombinationSuccess);
 
       // TODO: DEBUG CODE
       _keyHookManager.LoadKeyHookConfig(new KeyComboConfig(new[] { Keys.LControlKey, Keys.LShiftKey, Keys.K }));
 
-      _traContextMenu = new ContextMenu(new[]
-        {
-          new MenuItem("Profile Settings", TrayContextMenu_OnProfileSettings),
-          new MenuItem("-"),
-          new MenuItem("Open", Open),
-          new MenuItem("Exit", Exit),
-        });
+      var menuItems = GetProfileMenuItems();
+      menuItems.Add(new MenuItem("-"));
+      menuItems.Add(new MenuItem("Open", Open));
+      menuItems.Add(new MenuItem("-"));
+      menuItems.Add(new MenuItem("Profile Settings", TrayContextMenu_OnProfileSettings));
+      menuItems.Add(new MenuItem("-"));
+      menuItems.Add(new MenuItem("Exit", Exit));
+
+      _traContextMenu = new ContextMenu(menuItems.ToArray());
 
       _trayIcon = new NotifyIcon
       {
@@ -53,6 +58,22 @@ namespace Windowmancer.UI
         ContextMenu = _traContextMenu,
         Visible = true
       };
+    }
+
+    private List<MenuItem> GetProfileMenuItems()
+    {
+      var list = new List<MenuItem>();
+      foreach (var p in _profileManager.Profiles)
+      {
+        var m = new MenuItem(p.Name, TrayContextMenu_OnProfileSelect);
+        if (p.Id == _profileManager.ActiveProfile.Id)
+        {
+          m.Checked = true;
+        }
+        m.Tag = p;
+        list.Add(m);
+      }
+      return list;
     }
 
     private void OnKeyCombinationSuccess()
@@ -68,11 +89,52 @@ namespace Windowmancer.UI
 
     private void Open(object sender, EventArgs e)
     {
-      var editor = new MainForm(_serviceResolver, _profileManager, _windowManager);
-      editor.Show();
+      _editor = new MainForm(_serviceResolver, _profileManager, _windowManager);
+      // Update our context menu profile selection on profile change
+      // in the editor.
+      _editor.ProfileListBox.SelectedIndexChanged += (s, ev) =>
+      {
+        var profile = (Profile)_editor.ProfileListBox.SelectedItem;
+        UncheckCheckedMenuItem();
+        foreach (MenuItem m in _traContextMenu.MenuItems)
+        {
+          if (m.Tag == profile)
+          {
+            m.Checked = true;
+            break;
+          }
+        }
+      };
+      _editor.ShowDialog();
+      _editor = null;
+    }
+
+    private void UncheckCheckedMenuItem()
+    {
+      foreach (MenuItem m in _traContextMenu.MenuItems)
+      {
+        if (m.Checked)
+        {
+          m.Checked = false;
+          break;
+        }
+      }
     }
 
     #region Events
+
+    public void TrayContextMenu_OnProfileSelect(object sender, EventArgs e)
+    {
+      // Uncheck previous item.
+      UncheckCheckedMenuItem();
+      var mi = ((MenuItem)sender);
+      mi.Checked = true;
+      _profileManager.UpdateActiveProfile(((Profile)mi.Tag).Id);
+      if (null != _editor)
+      {
+        _editor.UpdateActiveProfile(((Profile)mi.Tag));
+      }
+    }
 
     public void TrayContextMenu_OnProfileSettings(object sender, EventArgs e)
     {
