81a916d519a8f2e5a5bf3f734840d5a1cfec63ef
tabeyti <tabeyti@gmail.com>
2017-05-15 19:36:30 -0700

    JenkinsFile update for CI.

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,8 +1,11 @@
-node {
-  stage ('Checkout')
-    git credentialsId: 'git_creds', url: 'https://github.com/tabeyti/windowmancer.git'
+def final BUILD_CONFIG = "Release"
+def final PLATFORM = "x64"
 
-  stage ('Build')
-    bat 'nuget restore .\\src\\Windowmancer.sln'
-    // bat "python  .\\src\\Windowmancer.sln /p:Configuration=Release /p:Platform=\"x64\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+node {
+  stage ('Checkout') {
+    git credentialsId: 'git_creds', url: 'https://github.com/tabeyti/windowmancer.git'
+  }
+  stage ('Build') {
+    bat "python src/build.py -c ${config} -p ${platform}"
+  }
 }
