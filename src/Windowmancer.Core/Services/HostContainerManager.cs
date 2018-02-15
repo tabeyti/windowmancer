5bc63dab0f1e0717480e7d13ba1bffb4e7fb25ef
tabeyti <tabeyti@blizzard.com>
2018-02-14 17:32:59 -0800

    Missing file in last commit.

---
+++ src/Windowmancer.Core/Services/HostContainerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windowmancer.Core.Models;
+
+namespace Windowmancer.Core.Services
+{
+  public class HostContainerManager : IDisposable
+  {
+    public Profile ActiveProfile => _profileManager.ActiveProfile;
+
+    private readonly ProfileManager _profileManager;
+
+    #region Constructors
+
+    public HostContainerManager(ProfileManager profileManager)
+    {
+      _profileManager = profileManager;
+    }
+
+    #endregion Constructors
+
+    public void Dispose()
+    {
+    }
+  }
+}
